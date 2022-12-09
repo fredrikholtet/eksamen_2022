cf52132d100032cd1f6319b228ff6325c1af1068
fredrik <fredrik.a.holtet@gmail.com>
2022-12-09 15:33:00 +0100

    testing againt and check build

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,4 +1,8 @@
-
-resource "aws_s3_bucket" "analytichelvete" {
-  bucket = "analytics-${var.candidate_id}"
+resource " aws_s3_bucket_server_side_encryption_configuration" "_testigjen" {
+  bucket = aws_s3_bucket.analyticsbucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
 }
