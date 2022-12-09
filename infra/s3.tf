f767da0a37de1c2c6d1e1d2ce39a79bc66f441cc
fredrik <fredrik.a.holtet@gmail.com>
2022-12-09 15:04:01 +0100

    testing againt and check build

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,4 +1,4 @@
-resource " aws_s3_bucket_server_side_encryption_configuration" "Fred" {
+resource " aws_s3_bucket_server_side_encryption_configuration" "testing2build" {
   bucket = aws_s3_bucket.analyticsbucket.bucket
   rule {
     apply_server_side_encryption_by_default {
