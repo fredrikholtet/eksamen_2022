9631c096693f8c43c30ee119dd1ae78b030b70ab
fredrik <fredrik.a.holtet@gmail.com>
2022-12-09 17:16:21 +0100

    testing againt and check build

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,9 @@
 variable "candidate_id" {
   type = string
+  default = "1032"
 }
 
-variable "candidate_email"  {
+variable "candidate_email" {
   type = string
+  default = "frho015@student.kristiania.no"
 }
