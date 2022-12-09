54966f56a4c78bb9d878cc4c7ce3e32e43571996
fredrik <fredrik.a.holtet@gmail.com>
2022-12-09 15:11:06 +0100

    testing againt and check build

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,4 +1,10 @@
-
-resource "aws_s3_bucket" "analyticsbucket" {
-  bucket = "analytics-${var.candidate_id}"
+resource "aws_s3_bucket_server_side_encryption_configuration" "fredku3" {
+  bucket = aws_s3_bucket.analyticsbucket.bucket
 }
+
+
+
+
+
+
+
