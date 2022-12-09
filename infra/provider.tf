1383db3318c34a9331a7391ebc2dafa6a527297b
fredrik <fredrik.a.holtet@gmail.com>
2022-12-09 15:44:54 +0100

    testing againt and check build

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-      bucket = "pgr301-2021-terraform-state"
+      bucket = "analytics-1032-terraform-state"
       key    = "1032/terraform-in-pipeline.state"
       region = "eu-west-1"
     }
