f02986c4f5e0ed8b6a777e5d751aea53a78a560a
fredrik <fredrik.a.holtet@gmail.com>
2022-12-09 14:49:17 +0100

    testing againt and check build

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,6 +1,6 @@
 
 resource " aws_s3_bucket_server_side_encryption_configuration" "example4exam" {
-  bucket = aws_s3_bucket
+  bucket = aws_s3_bucket.analyticsbucket.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm    = "AES256"
